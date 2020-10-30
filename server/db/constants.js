a5271f5c086201b50467c372bb34e56376eb2a65
adityatripathiiit <adityatripathiball@gmail.com>
2020-10-31 02:01:28 +0530

    Added client and server

---
+++ server/db/constants.js
@@ -0,0 +1,5 @@
+'use strict';
+module.exports = {
+     'MONGO_URI' : "mongodb+srv://linkedin:linkedin@linkedin.ruqk9.mongodb.net/linkedin?retryWrites=true&w=majority",
+     'MONGO_DB_NAME' : "linkedin"
+};
---
+++ server/db/db.js
@@ -0,0 +1,22 @@
+
+const mongoose = require('mongoose');
+const MONGO_URL = require('./constants.js').MONGO_URI;
+
+const connectMongo = async() => {
+    try{
+        await mongoose.connect(MONGO_URL, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+        });
+        console.log("connected");
+    }
+    catch(err){
+        console.log(err); 
+        throw (err);
+    }
+}
+
+
+module.exports = connectMongo;
